43ca674697837a456687df74ee3ab49783030837
Alejandro <amalanch@ucsd.edu>
2025-04-13 20:00:21 -0700

    batman

---
+++ src/test/java/com/example/resumeATS/ResumeAtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resumeATS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeAtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
